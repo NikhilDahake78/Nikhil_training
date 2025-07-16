02dfca8962462fbb79fd188b0f46d375de8f7e25
Nikhil Dahake <nikhil.dahake@agiliad.local>
2025-07-16 15:47:18 +0530

    adding test 3 with relevant changes

--- UI_automation_exercise/conftest.py
+++ UI_automation_exercise/conftest.py
@@ -1,7 +1,16 @@
-import pytest
-from playwright.sync_api import Playwright
+""" This is conftest.py """
 from datetime import datetime
+from logging import exception
+
+import pytest
 from utils.Logging import Log
+from utils.user import user
+from pageObjects.login_sign_up import LoginSignUpPage
+
+
+# global variable
+is_user_created = False
+user_id = 'user_1'
 
 def pytest_configure(config):
     """
@@ -30,6 +39,9 @@ def pytest_configure(config):
 
 
 def pytest_addoption(parser):
+    """
+    Adding customize arguments.
+    """
     parser.addoption(
         "--Browser",
         action="store",
@@ -45,15 +57,24 @@ def pytest_addoption(parser):
         choices=("True", "False")
     )
 
+
 @pytest.fixture
 def my_browser(playwright, request):
+    """
+    :param playwright: default
+    :param request: default
+    :return: browser page
+    """
     browser_name = request.config.getoption("Browser")
     headless_status = True if request.config.getoption("headless") == "True" else False
 
     Log.title("Setup")
     Log.info(f"Selected browser: {browser_name}")
-    Log.info(f"Test will run in {headless if headless_status == 'False' else 'headed'} mode")
-    Log.info("Opening browser browser.")
+    Log.info(f"Test will run in {'headless' if headless_status == 'False' else 'headed'} mode")
+    Log.info("Opening browser.")
+
+
+    browser = ''
 
     if browser_name == 'chrome':
         browser = playwright.chromium.launch(headless=headless_status)
@@ -66,7 +87,56 @@ def my_browser(playwright, request):
     yield page
 
     Log.title("Teardown")
+
+    # deleting user
+    if is_user_created:
+
+        try:
+            user.login_user(page, user_id)
+            # loginPage = LoginSignUpPage(page)
+            # loginPage.user_login(user_id)
+            Log.info(f"Deleting user")
+            user.delete_user(page)
+        except Exception as e :
+            Log.warning(f"Exception is: {e}")
+
+
     Log.info("Closing browser.")
     page.close()
     context.close()
     browser.close()
+
+
+@pytest.fixture
+def browser_with_user(my_browser):
+    """
+    :return: browser page after creating a user[id: user_1]
+    """
+
+    Log.info(f"Creating new user for testing: ID: {user_id}")
+    user.create_new_user(my_browser, user_id)
+    user.logout_user(my_browser, user_id)
+
+    yield my_browser
+
+    global is_user_created
+    is_user_created = True
+
+
+
+@pytest.fixture
+def dummy1():
+    print("\nwe are inside dummy1 setup")
+
+    yield
+
+    print("\nthis is dummy 1 teardown")
+
+
+@pytest.fixture
+def dummy2(dummy1):
+    print("\nwe are inside dummy2 setup")
+
+    yield
+
+    print("\nthis is dummy 2 teardown")
--- UI_automation_exercise/test/test_login_page.py
+++ UI_automation_exercise/test/test_login_page.py
@@ -1,50 +1,75 @@
+"""
+This is tests file.
+"""
 import json
 from time import sleep
 from playwright.sync_api import Playwright, expect, Page
 from pageObjects.home import HomePage
 from pageObjects.login_sign_up import LoginSignUpPage
 from utils.Logging import Log
-from utils.user import *
+from utils.user import user
 
 
 def test_register_user(my_browser):
+    """ test 1"""
     user_id = "user_1"
 
     Log.title("test_register_user")
     Log.info(f"Creating User with user_id : {user_id}")
 
     # create user
-    create_new_user(my_browser, user_id)
+    user.create_new_user(my_browser, user_id)
 
     Log.info("Deleting User")
     homePage = HomePage(my_browser)
     homePage.varify_page_with_user_sign_in(user_id)
 
     # delete user
-    delete_user(my_browser, user_id)
+    user.delete_user(my_browser, user_id)
 
 
-def test_login_user_with_correct_credentials(my_browser):
+def test_login_user_with_correct_credentials(browser_with_user):
+    """ test 2"""
 
     # create user
     user_id = "user_1"
 
     Log.title("test_login_user_with_correct_credentials")
 
-    Log.info("Creating a user for testing.")
-    create_new_user(my_browser, user_id)
-    logout_user(my_browser, user_id)
-
     # login as created user
     Log.info(f"Logging in as {user_id}.")
-    login_user(my_browser, user_id)
+    user.login_user(browser_with_user, user_id)
 
     # deleting user
     Log.info(f"Deleting user")
-    delete_user(my_browser, user_id)
+    user.delete_user(browser_with_user, user_id)
 
 
-def test_user_delete(my_browser):
+def test_logout_user(browser_with_user):
+    """ test 4"""
     user_id = "user_1"
-    login_user(my_browser, user_id)
-    delete_user(my_browser, user_id)
+
+    Log.title("Test user logout")
+
+    # login as created user
+    Log.info(f"Logging in as {user_id}.")
+    user.login_user(browser_with_user, user_id)
+
+    # logout user
+    Log.info(f"logging out as {user_id} ")
+    user.logout_user(browser_with_user)
+
+    # check
+    Log.info("Varifying if login page is visible after logout.")
+    loginPage = LoginSignUpPage(browser_with_user)
+    loginPage.varify_login_option()
+
+    # test result
+    Log.info('\t\tTest passed.')
+
+
+def test_user_delete(browser_with_user):
+    """ """
+    user_id = "user_1"
+    user.login_user(browser_with_user, user_id)
+    user.delete_user(browser_with_user, user_id)
